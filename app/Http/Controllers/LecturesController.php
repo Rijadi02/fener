e43b802d57ef420c0888520bf83ed27c4a61a513
Rijadi's Server <adiveni0@gmail.com>
2021-06-07 10:43:03 +0200

    added models, contorllers, resources

---
+++ app/Http/Controllers/LecturesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lectures;
+use Illuminate\Http\Request;
+
+class LecturesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lectures  $lectures
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lectures $lectures)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lectures  $lectures
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lectures $lectures)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Lectures  $lectures
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lectures $lectures)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lectures  $lectures
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lectures $lectures)
+    {
+        //
+    }
+}
