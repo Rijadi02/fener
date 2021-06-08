bedce0539e3165fd25dcd7b83313de5be6e5adcf
Rijadi's Server <adiveni0@gmail.com>
2021-06-08 15:10:32 +0200

    api controllers

---
+++ app/Http/Controllers/API/LecturesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Lectures;
+use Illuminate\Http\Request;
+
+class LecturesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lectures  $lectures
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lectures $lectures)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Lectures  $lectures
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lectures $lectures)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lectures  $lectures
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lectures $lectures)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/API/LibraryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Library;
+use Illuminate\Http\Request;
+
+class LibraryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Library  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Library $library)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Library  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Library $library)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Library  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Library $library)
+    {
+        //
+    }
+}
