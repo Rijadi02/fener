9a52124263f42383594eeac808e3fe5a0d8b5759
Rijadi's Server <adiveni0@gmail.com>
2021-12-06 11:12:39 +0100

    logout api

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('user/logout', [UserController::class, 'logout']);
+Route::post('logout', [UserController::class, 'logout']);
 
 //register controller
 Route::post('register', [RegisterController::class, "create_api"]);
